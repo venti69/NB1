a6b14f63976664b388fdae10eca1e7528a5edae6
venti69 <nagyvatir@gmail.com>
2023-11-24 09:12:31 +0100

    Add project files.

---
+++ MB1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace MB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.Nev = new System.Windows.Forms.Label();
+            this.Stadion = new System.Windows.Forms.Label();
+            this.Telepules = new System.Windows.Forms.Label();
+            this.Alapitas = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Feltolt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ID.Location = new System.Drawing.Point(12, 68);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(24, 16);
+            this.ID.TabIndex = 1;
+            this.ID.Text = "ID:";
+            // 
+            // Nev
+            // 
+            this.Nev.AutoSize = true;
+            this.Nev.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Nev.Location = new System.Drawing.Point(12, 110);
+            this.Nev.Name = "Nev";
+            this.Nev.Size = new System.Drawing.Size(36, 16);
+            this.Nev.TabIndex = 2;
+            this.Nev.Text = "Név:";
+            // 
+            // Stadion
+            // 
+            this.Stadion.AutoSize = true;
+            this.Stadion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Stadion.Location = new System.Drawing.Point(12, 176);
+            this.Stadion.Name = "Stadion";
+            this.Stadion.Size = new System.Drawing.Size(57, 16);
+            this.Stadion.TabIndex = 3;
+            this.Stadion.Text = "Stadion:";
+            // 
+            // Telepules
+            // 
+            this.Telepules.AutoSize = true;
+            this.Telepules.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Telepules.Location = new System.Drawing.Point(12, 244);
+            this.Telepules.Name = "Telepules";
+            this.Telepules.Size = new System.Drawing.Size(72, 16);
+            this.Telepules.TabIndex = 4;
+            this.Telepules.Text = "Település:";
+            // 
+            // Alapitas
+            // 
+            this.Alapitas.AutoSize = true;
+            this.Alapitas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Alapitas.Location = new System.Drawing.Point(12, 300);
+            this.Alapitas.Name = "Alapitas";
+            this.Alapitas.Size = new System.Drawing.Size(60, 16);
+            this.Alapitas.TabIndex = 5;
+            this.Alapitas.Text = "Alapítás:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(84, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(84, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(84, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(84, 238);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(161, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(84, 294);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(161, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(265, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(523, 254);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Feltolt
+            // 
+            this.Feltolt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Feltolt.Location = new System.Drawing.Point(229, 344);
+            this.Feltolt.Name = "Feltolt";
+            this.Feltolt.Size = new System.Drawing.Size(130, 36);
+            this.Feltolt.TabIndex = 12;
+            this.Feltolt.Text = "Feltölt";
+            this.Feltolt.UseVisualStyleBackColor = true;
+            this.Feltolt.Click += new System.EventHandler(this.Feltolt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Feltolt);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Alapitas);
+            this.Controls.Add(this.Telepules);
+            this.Controls.Add(this.Stadion);
+            this.Controls.Add(this.Nev);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Nev;
+        private System.Windows.Forms.Label Stadion;
+        private System.Windows.Forms.Label Telepules;
+        private System.Windows.Forms.Label Alapitas;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Feltolt;
+    }
+}
+
---
+++ MB1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace MB1
+{
+    public partial class Form1 : Form
+    {
+        public static MySqlConnection conn = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cStr = "server=localhost;database=nb1;username=root;password=";
+            conn = new MySqlConnection(cStr);
+            conn.Open();
+            dataGridViewFeltolt();
+            conn.Close();
+        }
+
+        private void Feltolt_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string neve = textBox2.Text;
+            string sta = textBox3.Text;
+            string tel = textBox4.Text;
+            string alap = textBox5.Text;
+            string connStr = $"insert into csapatok (id, neve, stadion, telepules, alapitas) values ('{id}', '{neve}', '{sta}', '{tel}', '{alap}')";
+            MySqlCommand cmd = new MySqlCommand(connStr, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            dataGridViewFeltolt();
+            conn.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+        private void dataGridViewFeltolt()
+        {
+            string ConnStri = "Select id, neve, stadion, telepules, alapitas tantargy from csapatok";
+            MySqlCommand cmd = new MySqlCommand(ConnStri, conn);
+            MySqlDataAdapter mda = new MySqlDataAdapter();
+            mda.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            mda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+
+            textBox1.Text = selectedRow.Cells[0].Value.ToString();
+            textBox2.Text = selectedRow.Cells[1].Value.ToString();
+            textBox3.Text = selectedRow.Cells[2].Value.ToString();
+            textBox4.Text = selectedRow.Cells[3].Value.ToString();
+            textBox5.Text = selectedRow.Cells[4].Value.ToString();
+        }
+
+    }
+}
